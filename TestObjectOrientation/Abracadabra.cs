729b8e30237acc8098e49e010c8f39641252eecf
Tunji Oyewole <t.oyewole23@outlook.com>
2021-05-24 16:01:15 -0400

    Add project files.

---
+++ TestObjectOrientation/Abracadabra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestObjectOrientation
+{
+    class Abracadabra
+    {
+        public int Vavavoom;
+
+        public bool Lala(int floq)
+        {
+            if (floq < Vavavoom)
+            {
+                Vavavoom += floq;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TestObjectOrientation/Pizzazz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestObjectOrientation
+{
+    class Pizzazz
+    {
+        public int Zippo;
+
+        public void Bamboo(int eek)
+        {
+            Zippo += eek;
+        }
+    }
+}
